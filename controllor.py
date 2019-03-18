bb9009871fe40a94f0fa21c08cce69114bb3ff41
Gladysgong <531404376@qq.com>
2019-03-18 17:04:44 +0800

    controllor and diskpatch

---
+++ controllor.py
@@ -0,0 +1,85 @@
+#! /usr/bin/env python
+# coding=utf-8
+import subprocess
+import pymysql
+import time
+from lib import logUtils
+from qoconf import *
+
+# db = pymysql.connect(database_host,database_user,database_pass,database_data)
+# cursor = db.cursor()
+log_fd = open('log/log', 'a')
+
+
+def get_running_id(cursor, loginfo):
+    # sql = "SELECT id FROM {_table} where status=2 and runningIP='{_ip}' limit 1".format(_table=database_table,_ip=local_ip)
+    sql = "SELECT id FROM {_table} where status=2 and svIP='{_ip}' limit 1".format(_table=database_table, _ip=local_ip)
+    loginfo.log_info(sql)
+    cursor.execute(sql)
+    data = cursor.fetchone()
+    if data is not None:
+        return data[0]
+    return -1
+
+
+def get_my_id(cursor, loginfo):
+    # sql = "SELECT id FROM {_table} where status=1 and runningIP='{_ip}' limit 1".format(_table=database_table,_ip=local_ip)
+    sql = "SELECT id FROM {_table} where status=1 and svIP='{_ip}' limit 1".format(_table=database_table, _ip=local_ip)
+    loginfo.log_info(sql)
+    cursor.execute(sql)
+    data = cursor.fetchone()
+    if data is not None:
+        return data[0]
+    return -1
+
+
+def get_cancel_id(cursor, loginfo):
+    # sql = "SELECT id FROM {_table} where status=6 and runningIP='{_ip}' limit 1".format(_table=database_table,_ip=local_ip)
+    sql = "SELECT id FROM {_table} where status=6 and svIP='{_ip}' limit 1".format(_table=database_table, _ip=local_ip)
+    loginfo.log_info(sql)
+    cursor.execute(sql)
+    data = cursor.fetchone()
+    if data is not None:
+        return data[0]
+    return -1
+
+
+def main():
+    log_fd = open('log/log', 'a')
+    loginfo = logUtils.logutil(0)
+    task_list = {}
+    while True:
+        db = pymysql.connect(database_host, database_user, database_pass, database_data)
+        cursor = db.cursor()
+        time.sleep(2)
+        running_id = get_running_id(cursor, loginfo);
+        if (running_id != -1):
+            continue
+        for (k, v) in task_list.items():
+            if (v.poll() != None):
+                del task_list[k]
+        mission_id = get_my_id(cursor, loginfo)
+        loginfo.log_info('mission_id' + str(mission_id))
+        if mission_id is not -1:
+            loginfo.log_info("task start")
+            child = subprocess.Popen(['/usr/local/bin/python2', 'run.py', '%d' % mission_id], shell=False,
+                                     stdout=log_fd, stderr=log_fd, cwd=autoqps_path)
+            task_list[mission_id] = child
+        cancel_id = get_cancel_id(cursor, loginfo)
+        if cancel_id is -1:
+            continue
+        if cancel_id in task_list:
+            task_list[cancel_id].send_signal(10)
+            loginfo.log_info('cancel:' + str(task_list))
+        else:
+            sql = "UPDATE {_table} set status = 5 WHERE id = {_cancel_id}".format(_table=database_table,
+                                                                                  _cancel_id=cancel_id)
+            cursor.execute(sql)
+            try:
+                db.commit()
+            except:
+                db.rollback()
+
+
+if __name__ == '__main__':
+    main()
---
+++ disk_patcher.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+# import MySQLdb
+import time
+import pymysql
+from lib import logUtils
+
+# status：任务的状态（0:未开始；1:已分配；2正在运行；3:出错停止；4:已完成；5:任务取消；6:准备取消；7:端口评测的状态）
+
+# database_host = "127.0.0.1"
+database_host = "10.134.120.30"
+database_data = "demo_pro"
+database_table = "picEval_imagetaskinfo"
+database_user = "root"
+database_pass = "Websearch@qa66"
+
+server_nodes = ['10.144.82.27']
+
+
+def check_new_task(cursor):
+    cursor.execute("SELECT id FROM {_table} where status=0 ORDER BY create_time limit 1".format(_table=database_table))
+    data = cursor.fetchone()
+    if data == None:
+        return -1
+    return data[0]
+
+
+def get_node(cursor):
+    # cursor.execute("select runningIP from {_table} where status=2 or status=1".format(_table=database_table))
+    cursor.execute("select svIP from {_table} where status=2 or status=1".format(_table=database_table))
+    data = cursor.fetchall()
+    used_ip = []
+    for ip in data:
+        used_ip.append(ip[0])
+    for node in server_nodes:
+        if node not in used_ip:
+            return node
+    return ""
+
+
+def do_mission(mission_id, ip, db):
+    cursor = db.cursor()
+    # sql = "UPDATE {_table} set runningIP='{_ip}', status=1 where id={_mission_id}".format(_table=database_table, _ip=ip,
+    sql = "UPDATE {_table} set svIP='{_ip}', status=1 where id={_mission_id}".format(_table=database_table, _ip=ip,
+                                                                                          _mission_id=mission_id)
+    try:
+        cursor.execute(sql)
+        db.commit()
+    except:
+        db.rollback()
+        return 1
+    return 0
+
+
+def main():
+    log_info = logUtils.logutil('qo')
+    while True:
+        db = pymysql.connect(database_host, database_user, database_pass, database_data)
+        cursor = db.cursor()
+        mission_id = check_new_task(cursor)
+        if mission_id is -1:
+            time.sleep(1)
+            log_info.log_info('There is no task')
+            continue
+        ip = get_node(cursor)
+        print(ip)
+        log_info.log_info('getnodeip' + ip)
+        if ip is "":
+            time.sleep(30)
+            log_info.log_info("new task %d, but all servers are busy" % mission_id)
+            continue
+        log_info.log_info("task %d will run on %s" % (mission_id, ip))
+        ret = do_mission(mission_id, ip, db)
+        log_info.log_info("return:%d" % ret)
+
+
+# db = MySQLdb.connect(database_host,database_user,database_pass,database_data)
+# cursor = db.cursor()
+# cursor.execute("SELECT runningIP FROM AutoQPS where status=0 ORDER BY create_time")
+# data = cursor.fetchall()
+# print data
+
+if __name__ == '__main__':
+    main()
