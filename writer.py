7766a2c3e9d8df11a600dbabcad56b4aeb261640
Gladysgong <531404376@qq.com>
2019-04-04 17:24:24 +0800

    save json

--- writer.py
+++ writer.py
@@ -11,14 +11,3 @@ async def saveImg(content, filepath):
     async with aiofiles.open(filepath, 'wb+') as f:
         await f.write(content)
 
-def saveJson1(content, filepath):
-    with open(filepath, 'w') as f:
-        f.write(json.dumps(content))
-
-def saveImg1(content, filepath):
-    with open(filepath, 'wb+') as f:
-        f.write(content)
-
-# def saveImg(content, filepath):
-#     with aiofiles.open(filepath, 'w') as f:
-#         f.write(content)
